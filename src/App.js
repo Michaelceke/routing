1d29adc2130efc69fa31cf632d54b6fede638fb9
Michaelceke <ekemicheal@yahoo.com>
2021-08-30 14:40:57 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Link,NavLink} from "react-router-dom"
+import Route from "react-router-dom/Route";
+
+
+
+const User=({match})=>{
+    return (<h1> Welcome user oooo {match.params.username}</h1>)
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li><Link to="/"> Home</Link></li>
+          <li> <NavLink to="/about" activeStyle={{color:"red"}}> About</NavLink></li>
+          <li> <Link to="/user/john"> john</Link></li>
+        </ul>
+       
+        <Route path="/"  exact render={
+          ()=>{
+            return (<h1>Welcome Home</h1>)
+          }
+        }/>
+
+       <Route path="/about" exact strict render={
+          ()=>{
+            return (<h1>about</h1>)
+          }
+        }/>
+
+         <Route path="/user/:username" exact strict component={User}
+        />
+     
+     </div>
+    </Router>
+    
   );
 }
 
